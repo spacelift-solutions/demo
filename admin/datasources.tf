7d267223f48943c30790ae9c317220264c9f997b
jubran <jubrann@spacelift.io>
2024-10-25 11:26:41 -0500

    grants admin stack gcp scopes

---
+++ admin/datasources.tf
@@ -0,0 +1,1 @@
+data "spacelift_current_stack" "admin" {}
--- admin/integrations.tf
+++ admin/integrations.tf
@@ -3,3 +3,13 @@ resource "spacelift_aws_integration" "demo" {
   role_arn = var.role_arn
   space_id = "root"
 }
+
+resource "spacelift_gcp_service_account" "admin" {
+  stack_id = data.spacelift_current_stack.admin.id
+
+  token_scopes = [
+    "https://www.googleapis.com/auth/compute",
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/devstorage.full_control",
+  ]
+}
