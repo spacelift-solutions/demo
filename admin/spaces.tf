2444596c8a66522c4b826c4a83f3f73411bfe8f3
jubran <jubrann@spacelift.io>
2024-10-25 17:45:50 -0500

    refactors spaces structure

--- admin/spaces.tf
+++ admin/spaces.tf
@@ -1,149 +1,131 @@
-resource "spacelift_space" "ansible" {
+resource "spacelift_space" "aws" {
+  name             = "aws"
+  inherit_entities = true
+  parent_space_id  = "root"
+}
+
+resource "spacelift_space" "aws-ansible" {
   name             = "ansible"
-  parent_space_id  = "root"
   inherit_entities = true
+  parent_space_id  = spacelift_space.aws.id
 }
 
-resource "spacelift_space" "ansible-aws" {
-  name             = "aws"
-  parent_space_id  = spacelift_space.ansible.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "ansible-gcp" {
-  name             = "gcp"
-  parent_space_id  = spacelift_space.ansible.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "ansible-azure" {
-  name             = "azure"
-  parent_space_id  = spacelift_space.ansible.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "cloudformation" {
+resource "spacelift_space" "aws-cloudformation" {
   name             = "cloudformation"
-  parent_space_id  = "root"
   inherit_entities = true
+  parent_space_id  = spacelift_space.aws.id
 }
 
-resource "spacelift_space" "kubernetes" {
+resource "spacelift_space" "aws-kubernetes" {
   name             = "kubernetes"
-  parent_space_id  = "root"
   inherit_entities = true
+  parent_space_id  = spacelift_space.aws.id
 }
 
-resource "spacelift_space" "kubernetes-aws" {
-  name             = "aws"
-  parent_space_id  = spacelift_space.kubernetes.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "kubernetes-gcp" {
-  name             = "gcp"
-  parent_space_id  = spacelift_space.kubernetes.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "kubernetes-azure" {
-  name             = "azure"
-  parent_space_id  = spacelift_space.kubernetes.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "opentofu" {
+resource "spacelift_space" "aws-opentofu" {
   name             = "opentofu"
-  parent_space_id  = "root"
   inherit_entities = true
+  parent_space_id  = spacelift_space.aws.id
 }
 
-resource "spacelift_space" "opentofu-aws" {
-  name             = "aws"
-  parent_space_id  = spacelift_space.opentofu.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "opentofu-gcp" {
-  name             = "gcp"
-  parent_space_id  = spacelift_space.opentofu.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "opentofu-azure" {
-  name             = "azure"
-  parent_space_id  = spacelift_space.opentofu.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "pulumi" {
+resource "spacelift_space" "aws-pulumi" {
   name             = "pulumi"
-  parent_space_id  = "root"
   inherit_entities = true
+  parent_space_id  = spacelift_space.aws.id
 }
 
-resource "spacelift_space" "pulumi-aws" {
-  name             = "aws"
-  parent_space_id  = spacelift_space.pulumi.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "pulumi-gcp" {
-  name             = "gcp"
-  parent_space_id  = spacelift_space.pulumi.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "pulumi-azure" {
-  name             = "azure"
-  parent_space_id  = spacelift_space.pulumi.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "terraform" {
+resource "spacelift_space" "aws-terraform" {
   name             = "terraform"
-  parent_space_id  = "root"
   inherit_entities = true
+  parent_space_id  = spacelift_space.aws.id
 }
 
-resource "spacelift_space" "terraform-aws" {
-  name             = "aws"
-  parent_space_id  = spacelift_space.terraform.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "terraform-gcp" {
-  name             = "gcp"
-  parent_space_id  = spacelift_space.terraform.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "terraform-azure" {
-  name             = "azure"
-  parent_space_id  = spacelift_space.terraform.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "terragrunt" {
+resource "spacelift_space" "aws-terragrunt" {
   name             = "terragrunt"
-  parent_space_id  = "root"
   inherit_entities = true
+  parent_space_id  = spacelift_space.aws.id
 }
 
-resource "spacelift_space" "terragrunt-aws" {
-  name             = "aws"
-  parent_space_id  = spacelift_space.terragrunt.id
-  inherit_entities = true
-}
-
-resource "spacelift_space" "terragrunt-gcp" {
+resource "spacelift_space" "gcp" {
   name             = "gcp"
-  parent_space_id  = spacelift_space.terragrunt.id
   inherit_entities = true
+  parent_space_id  = "root"
 }
 
-resource "spacelift_space" "terragrunt-azure" {
-  name             = "azure"
-  parent_space_id  = spacelift_space.terragrunt.id
+resource "spacelift_space" "gcp-ansible" {
+  name             = "ansible"
   inherit_entities = true
+  parent_space_id  = spacelift_space.gcp.id
+}
+
+resource "spacelift_space" "gcp-kubernetes" {
+  name             = "kubernetes"
+  inherit_entities = true
+  parent_space_id  = spacelift_space.gcp.id
+}
+
+resource "spacelift_space" "gcp-opentofu" {
+  name             = "opentofu"
+  inherit_entities = true
+  parent_space_id  = spacelift_space.gcp.id
+}
+
+resource "spacelift_space" "gcp-pulumi" {
+  name             = "pulumi"
+  inherit_entities = true
+  parent_space_id  = spacelift_space.gcp.id
+}
+
+resource "spacelift_space" "gcp-terraform" {
+  name             = "terraform"
+  inherit_entities = true
+  parent_space_id  = spacelift_space.gcp.id
+}
+
+resource "spacelift_space" "gcp-terragrunt" {
+  name             = "terragrunt"
+  inherit_entities = true
+  parent_space_id  = spacelift_space.gcp.id
+}
+
+resource "spacelift_space" "azure" {
+  name             = "azure"
+  inherit_entities = true
+  parent_space_id  = "root"
+}
+
+resource "spacelift_space" "azure-ansible" {
+  name             = "ansible"
+  inherit_entities = true
+  parent_space_id  = spacelift_space.azure.id
+}
+
+resource "spacelift_space" "azure-kubernetes" {
+  name             = "kubernetes"
+  inherit_entities = true
+  parent_space_id  = spacelift_space.azure.id
+}
+
+resource "spacelift_space" "azure-opentofu" {
+  name             = "opentofu"
+  inherit_entities = true
+  parent_space_id  = spacelift_space.azure.id
+}
+
+resource "spacelift_space" "azure-pulumi" {
+  name             = "pulumi"
+  inherit_entities = true
+  parent_space_id  = spacelift_space.azure.id
+}
+
+resource "spacelift_space" "azure-terraform" {
+  name             = "terraform"
+  inherit_entities = true
+  parent_space_id  = spacelift_space.azure.id
+}
+
+resource "spacelift_space" "azure-terragrunt" {
+  name             = "ansible"
+  inherit_entities = true
+  parent_space_id  = spacelift_space.azure.id
 }
