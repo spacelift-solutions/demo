a230b9e50f4a90810fa0c3bb4a7b214309f3d2b6
jubran <jubrann@spacelift.io>
2024-11-01 10:35:28 -0500

    fixes naming convention

--- admin/contexts.tf
+++ admin/contexts.tf
@@ -19,7 +19,7 @@ resource "spacelift_environment_variable" "gcp" {
 
 # #Create ansible context
 
-resource "spacelift_context" "ansible-context" {
+resource "spacelift_context" "ansible_context" {
   description = "Context for Ansible stacks"
   name        = "Ansible context"
   space_id    = spacelift_space.aws-ansible.id
@@ -27,14 +27,14 @@ resource "spacelift_context" "ansible-context" {
 }
 
 resource "spacelift_environment_variable" "ansible_remote_user" {
-  context_id = spacelift_context.ansible-context.id
+  context_id = spacelift_context.ansible_context.id
   name       = "ANSIBLE_REMOTE_USER"
   value      = "ec2-user"
   write_only = false
 }
 
 resource "spacelift_environment_variable" "ansible_inventory" {
-  context_id = spacelift_context.ansible-context.id
+  context_id = spacelift_context.ansible_context.id
   name       = "ANSIBLE_INVENTORY"
   value      = "/mnt/workspace/inventory.ini"
   write_only = false
@@ -47,14 +47,14 @@ resource "tls_private_key" "rsa" {
 }
 
 resource "spacelift_mounted_file" "private_key" {
-  context_id    = spacelift_context.ansible-context.id
+  context_id    = spacelift_context.ansible_context.id
   relative_path = "id_rsa"
   content       = base64encode(nonsensitive(tls_private_key.rsa.private_key_pem))
   write_only    = true
 }
 
 resource "spacelift_mounted_file" "public_key" {
-  context_id    = spacelift_context.ansible-context.id
+  context_id    = spacelift_context.ansible_context.id
   relative_path = "id_rsa.pub"
   content       = base64encode(tls_private_key.rsa.public_key_openssh)
   write_only    = true
@@ -62,7 +62,7 @@ resource "spacelift_mounted_file" "public_key" {
 
 
 resource "spacelift_environment_variable" "ansible_private_key_file" {
-  context_id = spacelift_context.ansible-context.id
+  context_id = spacelift_context.ansible_context.id
   name       = "ANSIBLE_PRIVATE_KEY_FILE"
   value      = "/mnt/workspace/id_rsa"
   write_only = false
