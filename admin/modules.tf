025c8d3582f73cf3417f6dd2516f998c31683ca2
Joey Stout <joey@apollorion.com>
2024-11-05 17:53:20 -0500

    feat: add new plugin (#35)

--- admin/modules.tf
+++ admin/modules.tf
@@ -11,3 +11,17 @@ resource "spacelift_module" "stacks_module" {
   public               = true
   labels               = ["module"]
 }
+
+resource "spacelift_module" "context_trigger_plugin_module" {
+  name                 = "plugin-context-trigger"
+  terraform_provider   = "spacelift"
+  administrative       = true
+  branch               = "main"
+  description          = "Plugin that triggers stacks on context changes."
+  repository           = "plugin-context-trigger"
+  space_id             = "root"
+  workflow_tool        = "OPEN_TOFU"
+  enable_local_preview = true
+  public               = true
+  labels               = ["module"]
+}
