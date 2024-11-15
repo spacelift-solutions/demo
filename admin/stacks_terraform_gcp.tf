d2a8aec8c7c496a8a322ed752bf8f1005b3127de
MarcusScipio <maring@spacelift.io>
2024-11-15 17:14:31 +0200

    fix: testing chmod

--- admin/stacks_terraform_gcp.tf
+++ admin/stacks_terraform_gcp.tf
@@ -237,6 +237,7 @@ module "stack_gcp_cloud_functions" {
   hooks = {
     before = {
       init = [
+        "chmod +x /mnt/workspace/source/terraform/gcp/gcp-environment/terraform/gcp/gcp-environment/scripts/package-deploy.sh",
         "/mnt/workspace/source/terraform/gcp/gcp-environment/terraform/gcp/gcp-environment/scripts/package-deploy.sh"
       ]
     }
