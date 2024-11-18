1a8599000e32398a2360e88eac834326df1f4de7
jubranNassar <jubrann@spacelift.io>
2024-11-18 10:08:38 -0600

    fix: worker pool output

--- admin/outputs.tf
+++ admin/outputs.tf
@@ -1,5 +1,5 @@
 output "ec2_worker_pool_private_key" {
-  value     = spacelift_worker_pool.aws_ec2_asg.private_key
+  value     = base64encode(tls_private_key.main.private_key_pem)
   sensitive = true
 }
 
