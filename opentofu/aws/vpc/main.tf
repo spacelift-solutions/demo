ea518e6e95ced3592340da0a2d5d5b3d8cf2b064
jubranNassar <jubrann@spacelift.io>
2024-11-18 11:55:29 -0600

    feat: creates internet gateway

--- opentofu/aws/vpc/main.tf
+++ opentofu/aws/vpc/main.tf
@@ -64,3 +64,30 @@ resource "aws_security_group" "allow_access" {
     Name = "dev-sg"
   }
 }
+
+resource "aws_internet_gateway" "dev_internet_gateway" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "dev_igw"
+  }
+}
+
+resource "aws_route_table" "dev_public_rt" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.dev_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.dev_internet_gateway.id
+}
+
+resource "aws_route_table_association" "dev_public_assoc" {
+  subnet_id      = aws_subnet.dev_public_subnet.id
+  route_table_id = aws_route_table.dev_public_rt.id
+}
