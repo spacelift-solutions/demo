90bf1a1e852b538209becfe5863d6c64fbcf49fd
jubranNassar <jubrann@spacelift.io>
2024-12-31 14:48:19 -0600

    fix: deploy spacelift controller via kubectl

--- worker-pools/kubernetes/aws/main.tf
+++ worker-pools/kubernetes/aws/main.tf
@@ -1,11 +1,15 @@
-resource "helm_release" "spacelift_kubernetes_workers" {
-  name = "spacelift-workerpool-controller"
+# resource "helm_release" "spacelift_kubernetes_workers" {
+#   name = "spacelift-workerpool-controller"
 
-  repository = "https://downloads.spacelift.io/helm"
-  chart      = "spacelift-workerpool-controller"
+#   repository = "https://downloads.spacelift.io/helm"
+#   chart      = "spacelift-workerpool-controller"
 
-  namespace        = "spacelift-worker-controller-system"
-  create_namespace = true
+#   namespace        = "spacelift-worker-controller-system"
+#   create_namespace = true
+# }
+
+resource "kubectl_manifest" "worker_pool_controller" {
+  yaml_body = file("./secret.yaml")
 }
 
 resource "kubectl_manifest" "worker_pool_secret" {
