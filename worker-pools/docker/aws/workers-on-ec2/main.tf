c61cc23ef5bcbaa90cdb3da2570d6b16c7dc70db
apollorion <joey@apollorion.com>
2025-02-28 15:50:47 -0500

    feat: use secrets manager here

--- worker-pools/docker/aws/workers-on-ec2/main.tf
+++ worker-pools/docker/aws/workers-on-ec2/main.tf
@@ -1,9 +1,21 @@
+resource "aws_secretsmanager_secret" "workerpool_config" {
+  name = "workerpool-config-${var.worker_pool_id}"
+}
+
+resource "aws_secretsmanager_secret_version" "workerpool_config" {
+  secret_id = aws_secretsmanager_secret.workerpool_config.id
+  secret_string = jsonencode({
+    SPACELIFT_TOKEN            = var.worker_pool_config,
+    SPACELIFT_POOL_PRIVATE_KEY = var.worker_pool_private_key
+  })
+}
+
 module "aws_ec2_asg_worker_pool" {
-  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=ratelimit-check"
+  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2"
 
   configuration = <<-EOT
-    export SPACELIFT_TOKEN="${var.worker_pool_config}"
-    export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
+    export SPACELIFT_TOKEN="$(aws secretsmanager get-secret-value --secret-id workerpool-config-${var.worker_pool_id} --query SecretString.SPACELIFT_TOKEN --output text)"
+    export SPACELIFT_POOL_PRIVATE_KEY="$(aws secretsmanager get-secret-value --secret-id workerpool-config-${var.worker_pool_id} --query SecretString.SPACELIFT_POOL_PRIVATE_KEY --output text)"
     export SPACELIFT_SENSITIVE_OUTPUT_UPLOAD_ENABLED=true
   EOT
 
