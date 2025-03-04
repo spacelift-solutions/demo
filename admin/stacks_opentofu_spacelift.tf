fe448b1e46862fb5de0cd166e05ab735f475445f
apollorion <joey@apollorion.com>
2025-03-04 18:31:19 -0500

    feat: spin up tofusible

---
+++ admin/stacks_opentofu_spacelift.tf
@@ -0,0 +1,26 @@
+module "stack_opentofu_spacelift_tofusible" {
+  source = "spacelift.io/spacelift-solutions/stacks-module/spacelift"
+
+  description     = "Stack that creates EC2 Servers"
+  name            = "Tofusible - Administrative"
+  repository_name = "tofusible"
+  space_id        = spacelift_space.aws_opentofu.id
+
+  auto_deploy = true
+
+  aws_integration = {
+    enabled = true
+    id      = spacelift_aws_integration.demo.id
+  }
+
+  environment_variables = {
+    AWS_DEFAULT_REGION = {
+      value     = "us-east-1"
+      sensitive = false
+    }
+  }
+
+  labels            = ["tofusible", "admin"]
+  project_root      = "stacks/admin"
+  repository_branch = "main"
+}
