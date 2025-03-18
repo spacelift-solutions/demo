1f293827d573897de92a7992e727c26fdb2b4fb1
apollorion <joey@apollorion.com>
2025-03-18 15:45:25 -0400

    feat: change module inputs

--- worker-pools/docker/aws/workers-on-ec2/main.tf
+++ worker-pools/docker/aws/workers-on-ec2/main.tf
@@ -1,13 +1,13 @@
-resource "aws_kms_key" "secure_strings" {}
+resource "aws_kms_key" "secure_env_vars" {}
 
 module "aws_ec2_asg_worker_pool" {
   source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=security-updates"
 
-  secure_strings = {
-    SPACELIFT_TOKEN = var.worker_pool_config,
+  secure_env_vars = {
+    SPACELIFT_TOKEN            = var.worker_pool_config,
     SPACELIFT_POOL_PRIVATE_KEY = var.worker_pool_private_key
   }
-  secure_strings_kms_key_id = aws_kms_key.secure_strings.arn
+  secure_env_vars_kms_key_id = aws_kms_key.secure_env_vars.arn
 
   configuration = <<-EOT
     export SPACELIFT_SENSITIVE_OUTPUT_UPLOAD_ENABLED=true
@@ -21,4 +21,9 @@ module "aws_ec2_asg_worker_pool" {
   spacelift_api_key_endpoint = var.spacelift_api_key_endpoint
   spacelift_api_key_id       = var.spacelift_api_key_id
   spacelift_api_key_secret   = var.spacelift_api_key_secret
+}
+
+moved {
+  from = aws_kms_key.secure_strings
+  to   = aws_kms_key.secure_env_vars
 }
