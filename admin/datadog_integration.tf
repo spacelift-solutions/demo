6b3b16a1e6aa9e7c39cee89a19f7d0a3fea98979
apollorion <joey@apollorion.com>
2025-04-04 14:04:30 -0400

    feat: logging support

--- admin/datadog_integration.tf
+++ admin/datadog_integration.tf
@@ -1,6 +1,6 @@
 module "datadog" {
-  source  = "spacelift-io/datadog/spacelift"
-  version = "0.2.3"
+  source  = "github.com/spacelift-io/terraform-spacelift-datadog?ref=logging-support"
+  # version = "0.2.3"
   # insert the 1 required variable here
   dd_api_key = var.dd_api_key
   dd_site    = var.dd_site
