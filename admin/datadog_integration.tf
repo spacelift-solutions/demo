2210a7722fe10e9eb89f07afe6f1834323f9c84c
jacobjSL <118396550+jacobjSL@users.noreply.github.com>
2025-05-13 13:03:00 -0700

    added s3 blueprint + fix datadog module reference (#115)

--- admin/datadog_integration.tf
+++ admin/datadog_integration.tf
@@ -1,5 +1,5 @@
 module "datadog" {
-  source  = "github.com/spacelift-io/terraform-spacelift-datadog?ref=logging-support"
+  source = "spacelift-io/datadog/spacelift"
   # version = "0.2.3"
   # insert the 1 required variable here
   dd_api_key = var.dd_api_key
