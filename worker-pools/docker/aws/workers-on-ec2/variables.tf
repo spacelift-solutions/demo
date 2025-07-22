e6a9c491a453dd41c78e7839608df2f965f4cf35
apollorion <joey@apollorion.com>
2025-07-22 11:11:30 -0400

    feat: try joes module update

--- worker-pools/docker/aws/workers-on-ec2/variables.tf
+++ worker-pools/docker/aws/workers-on-ec2/variables.tf
@@ -15,7 +15,6 @@ variable "worker_pool_id" {
   description = "ID of the worker pool"
 }
 
-
 variable "spacelift_api_key_endpoint" {
   type        = string
   description = "Full URL of the Spacelift API endpoint to use, eg. https://demo.app.spacelift.io"
@@ -34,17 +33,4 @@ variable "spacelift_api_key_secret" {
   sensitive   = true
   description = "Secret corresponding to the Spacelift API key to use"
   default     = null
-}
-
-variable "poweroff_delay" {
-  type        = number
-  description = "Number of seconds to wait before powering the EC2 instance off after the Spacelift launcher stopped"
-  default     = 60
-}
-
-variable "autoscaler_version" {
-  description = "Version of the autoscaler to deploy"
-  type        = string
-  default     = "v1.0.1"
-  nullable    = false
 }
