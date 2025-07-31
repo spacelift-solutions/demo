76bc4e86ab161afee5b825b6bd05560cb652a16c
jacobjSL <118396550+jacobjSL@users.noreply.github.com>
2025-07-30 19:11:46 -0700

    Update providers.tf with encyrption

--- opentofu/aws/vpc/providers.tf
+++ opentofu/aws/vpc/providers.tf
@@ -1,4 +1,18 @@
 terraform {
+  encryption {
+    key_provider "pbkdf2" "migration_key" {
+      passphrase    = "super-passphrase-hard-to-find"
+      key_length    = 32
+      salt_length   = 16
+      hash_function = "sha256"
+    }
+    method "aes_gcm" "secure_method" {
+      keys = key_provider.pbkdf2.migration_key
+    }
+    state {
+      method = method.aes_gcm.secure_method
+    }
+  }
   required_providers {
     aws = {
       source  = "opentofu/aws"
