bb385ebc7f962a0ba5a5ccb937c8f8d99e344e18
jacobjSL <118396550+jacobjSL@users.noreply.github.com>
2025-07-30 19:47:51 -0700

    changed dev to tofu

--- opentofu/aws/vpc/outputs.tf
+++ opentofu/aws/vpc/outputs.tf
@@ -1,8 +1,8 @@
 output "subnet_id" {
   description = "ID of the Subnet"
-  value       = aws_subnet.dev_public_subnet.id
+  value       = aws_subnet.tofu_public_subnet.id
 }
 
-output "dev_sg" {
+output "tofu_sg" {
   value = aws_security_group.allow_access.id
 }
