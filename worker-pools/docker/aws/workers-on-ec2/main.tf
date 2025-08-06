43f89274afc67fbce7055f4b6bc76b3a3e6eea9a
jubran nassar <jubrann@spacelift.io>
2025-08-05 22:20:06 -0500

    fix: update worker pool module

--- worker-pools/docker/aws/workers-on-ec2/main.tf
+++ worker-pools/docker/aws/workers-on-ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_kms_key" "secure_env_vars" {}
 
 module "aws_ec2_asg_worker_pool" {
-  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v5.0.0"
+  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v5.0.1"
 
   secure_env_vars = {
     SPACELIFT_TOKEN            = var.worker_pool_config,
