98c1d9fed509f3ff24ae90c96124149cb5f15eca
kal <kalyano@spacelift.io>
2025-08-22 18:20:15 -0400

    Update variables.tf

--- worker-pools/docker/gcp/kals-workers/variables.tf
+++ worker-pools/docker/gcp/kals-workers/variables.tf
@@ -1,5 +1,3 @@
-# Variables for Spacelift GCP Worker Pool Stack
-
 variable "gcp_project_id" {
   description = "GCP Project ID"
   type        = string
@@ -12,28 +10,19 @@ variable "gcp_region" {
   default     = "us-central1"
 }
 
-variable "network_name" {
-  description = "GCP Network name to use for worker pool"
-  type        = string
-  default     = "default"  # Use default network or specify custom
-}
-
 variable "service_account_email" {
   description = "Service account email for the worker instances"
   type        = string
-  # Set this in Spacelift as Environment variable: TF_VAR_service_account_email
-  # Find your service account in GCP Console → IAM & Admin → Service Accounts
 }
 
-# New variables for Spacelift worker pool credentials
 variable "spacelift_token" {
-  description = "Spacelift worker pool token - set as sensitive Spacelift variable"
+  description = "Spacelift worker pool token"
   type        = string
   sensitive   = true
 }
 
 variable "spacelift_pool_private_key" {
-  description = "Spacelift worker pool private key - set as sensitive Spacelift variable"
+  description = "Spacelift pool private key (base64 encoded)"
   type        = string
   sensitive   = true
 }
