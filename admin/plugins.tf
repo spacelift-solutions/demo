7764da5a8984d393b6256abf6f453f0ae115a86f
Joey Stout <joey@apollorion.com>
2025-12-22 15:03:06 -0500

    fix: all the version errors (#140)

--- admin/plugins.tf
+++ admin/plugins.tf
@@ -3,14 +3,3 @@ module "plugin_sops" {
   name     = "plugin_sops"
   space_id = "root"
 }
-
-module "plugin_infracost" {
-  source = "spacelift.io/spacelift-solutions/plugin-infracost/spacelift"
-
-  space_id          = "root"
-  infracost_api_key = var.infracost_api_key
-
-  policies = {
-    INFRACOST_DEFAULT = file("${path.module}/policies/plan/infracost_cost_restriction.rego")
-  }
-}
