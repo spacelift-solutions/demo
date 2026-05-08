6b078b68168b36285bc9f12a9ee40d8415393cd6
Jnesspace <jacobn@spacelift.io>
2026-05-08 17:21:00 -0400

    Update eks.tf (#147)

--- terraform/aws/eks/eks.tf
+++ terraform/aws/eks/eks.tf
@@ -58,7 +58,6 @@ module "auth" {
   source                    = "terraform-aws-modules/eks/aws//modules/aws-auth"
   version                   = "~> 20.0"
   manage_aws_auth_configmap = true
-  create_aws_auth_configmap = true
   aws_auth_roles = [
     {
       rolearn  = module.eks_blueprints_addons.karpenter.node_iam_role_arn
