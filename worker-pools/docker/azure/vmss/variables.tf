32793f45c9c697e41edf3cc54393063bb19c0d86
Joey Stout <joey@apollorion.com>
2026-06-29 13:13:08 -0400

    fix: Azure VMSS pool as static single worker (drop module autoscaler) (#158)

--- worker-pools/docker/azure/vmss/variables.tf
+++ worker-pools/docker/azure/vmss/variables.tf
@@ -15,24 +15,6 @@ variable "worker_pool_id" {
   description = "ID of the Spacelift worker pool."
 }
 
-variable "spacelift_api_key_endpoint" {
-  type        = string
-  description = "Full URL of the Spacelift API endpoint, eg. https://spacelift-solutions.app.spacelift.io"
-  default     = "https://spacelift-solutions.app.spacelift.io"
-}
-
-variable "spacelift_api_key_id" {
-  type        = string
-  description = "ID of the Spacelift API key used by the autoscaler."
-  sensitive   = true
-}
-
-variable "spacelift_api_key_secret" {
-  type        = string
-  description = "Secret of the Spacelift API key used by the autoscaler."
-  sensitive   = true
-}
-
 variable "location" {
   type        = string
   description = "Azure region for the worker pool resources."
@@ -45,16 +27,10 @@ variable "vmss_sku" {
   default     = "Standard_B2S"
 }
 
-variable "worker_pool_min_size" {
+variable "worker_count" {
   type        = number
-  description = "Minimum number of workers the autoscaler keeps running."
+  description = "Number of always-on workers in the (non-autoscaled) pool."
   default     = 1
-}
-
-variable "worker_pool_max_size" {
-  type        = number
-  description = "Maximum number of workers the autoscaler may create."
-  default     = 5
 }
 
 variable "admin_username" {
