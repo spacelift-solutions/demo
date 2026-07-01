8098d9d58f2a5b1646f8968043b0137a6a47651b
Jayden Stipek <55769079+Clause828@users.noreply.github.com>
2026-07-01 16:21:27 -0400

    AWS CloudWatch dashboard stack with github-auth context (#166)
    Authored-by: Jayden Stipek <jaydens@spacelift.io>

--- admin/contexts_github.tf
+++ admin/contexts_github.tf
@@ -4,3 +4,31 @@ resource "spacelift_context" "github_auth" {
   space_id    = spacelift_space.aws_opentofu.id
   labels      = ["autoattach:github-auth"]
 }
+
+resource "spacelift_environment_variable" "context_secret" {
+  context_id = spacelift_context.github_auth.id
+  name       = "CONTEXT_SECRET"
+  value      = "thisisasecret"
+  write_only = true
+}
+
+resource "spacelift_environment_variable" "context_public" {
+  context_id = spacelift_context.github_auth.id
+  name       = "CONTEXT_PUBLIC"
+  value      = "This should be visible"
+  write_only = false
+}
+
+resource "spacelift_mounted_file" "context_secret" {
+  context_id    = spacelift_context.github_auth.id
+  relative_path = "CONTEXT_SECRET"
+  content       = base64encode("thisisasecret")
+  write_only    = true
+}
+
+resource "spacelift_mounted_file" "context_public" {
+  context_id    = spacelift_context.github_auth.id
+  relative_path = "CONTEXT_PUBLIC"
+  content       = base64encode("This should be visible")
+  write_only    = false
+}
