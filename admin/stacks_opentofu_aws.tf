0f772ae7d941747e3813765aab5499fbae6571dd
Jayden Stipek <jaydens@spacelift.io>
2026-08-03 16:17:49 -0400

    feat: re-add wiz label, drop two-person review policy, fix auto_deploy typo

--- admin/stacks_opentofu_aws.tf
+++ admin/stacks_opentofu_aws.tf
@@ -207,7 +207,7 @@ module "stack_aws_cloudwatch_dashboard" {
     id      = spacelift_aws_integration.demo.id
   }
 
-  labels                = ["aws", "cloudwatch", "dashboard", "deletion-prevention", "require-project-tag"]
+  labels                = ["aws", "cloudwatch", "dashboard", "deletion-prevention", "require-project-tag", "wiz"]
   project_root          = "opentofu/aws/cloudwatch_dashboard"
   repository_branch     = "main"
   protect_from_deletion = true
@@ -225,7 +225,6 @@ module "stack_aws_cloudwatch_dashboard" {
   }
   #these are the policies that will be applied to this stack, they are defined in the admin/policies.tf file
   policies = {
-    TWO_PERSON_REVIEW  = spacelift_policy.approval_cloudwatch_dashboard.id
     NO_WEEKEND_DEPLOYS = spacelift_policy.no-weekend-deploys.id
   }
 }
