5dad1d3388e68d72a0fd72fd876abfc840280e37
Joey Stout <joey@apollorion.com>
2026-08-24 13:25:15 -0400

    feat(ec2): force an instance replacement on every run (#187)

--- opentofu/aws/ec2/main.tf
+++ opentofu/aws/ec2/main.tf
@@ -3,6 +3,11 @@ resource "aws_key_pair" "ssh_key" {
   public_key = file(var.public_key)
 }
 
+# Forces an instance replacement every run so plans always carry a cost diff.
+resource "terraform_data" "always_replace" {
+  triggers_replace = plantimestamp()
+}
+
 resource "aws_instance" "sd_instance" {
   ami                         = data.aws_ami.dev_server_ami.id
   instance_type               = var.instance_type
@@ -13,5 +18,9 @@ resource "aws_instance" "sd_instance" {
 
   tags = {
     Name = "dev-node"
+  }
+
+  lifecycle {
+    replace_triggered_by = [terraform_data.always_replace]
   }
 }
