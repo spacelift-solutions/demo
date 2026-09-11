916a6d1d27538cdef0acbe4a6934f88ac66e4ed4
Jubran <84308658+jubranNassar@users.noreply.github.com>
2026-09-11 13:39:58 -0500

    feat: create new SE default role (#195)

--- admin/roles.tf
+++ admin/roles.tf
@@ -3,3 +3,37 @@ resource "spacelift_role" "admin" {
   description = "Role with full administrative privileges"
   actions     = ["SPACE_ADMIN"]
 }
+
+resource "spacelift_idp_group_mapping" "solutions_engineering" {
+  name        = "solutions-engineering"
+  description = "GitHub team solutions-engineering"
+}
+resource "spacelift_role" "default_solutions_engineering_role" {
+  name        = "default solutions engineering role"
+  description = "Default role for solutions engineering team"
+
+  actions = [
+    "RUN_CANCEL",
+    "RUN_CANCEL_BLOCKING",
+    "RUN_COMMENT",
+    "RUN_CONFIRM",
+    "RUN_DISCARD",
+    "RUN_KILL",
+    "RUN_KILL_BLOCKING",
+    "RUN_PRIORITIZE_SET",
+    "RUN_RETRY",
+    "RUN_RETRY_BLOCKING",
+    "RUN_REVIEW",
+    "RUN_STOP",
+    "RUN_STOP_BLOCKING",
+    "RUN_TARGETED_REPLAN",
+    "RUN_TRIGGER",
+    "SPACE_READ",
+  ]
+}
+
+resource "spacelift_role_attachment" "default_solutions_engineering_role_attachment" {
+  idp_group_mapping_id = spacelift_idp_group_mapping.solutions_engineering.id
+  role_id              = spacelift_role.default_solutions_engineering_role.id
+  space_id             = "root"
+}
